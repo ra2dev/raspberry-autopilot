709514697940ad605669a7a9a7c77d07e3bc4645
ra2dev <raramanouski@gmail.com>
2023-04-23 16:56:04 +0200

    Test

--- Code/Server/servo.py
+++ Code/Server/servo.py
@@ -7,7 +7,7 @@ class Servo:
         self.PwmServo.setServoPulse(8,1500)
         self.PwmServo.setServoPulse(9,1500)
     def setServoPwm(self,channel,angle,error=10):
-        angle=int(angle)
+        angle=int(angle) + 90
         print ("\nAngle: ", angle)
         if channel=='0':
             self.PwmServo.setServoPulse(8,2500-int((angle+error)/0.09))
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     pwm=Servo()
     while True:
         try :
-            pwm.setServoPwm('0',44)
-            pwm.setServoPwm('1',44)
+            pwm.setServoPwm('0',90)
+            pwm.setServoPwm('1',90)
         except KeyboardInterrupt:
             print ("\nEnd of program")
             break
